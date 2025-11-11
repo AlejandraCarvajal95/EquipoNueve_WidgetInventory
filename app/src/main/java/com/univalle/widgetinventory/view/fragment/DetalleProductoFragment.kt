f7c7845e54f161c83a376100b40fe5e0e0d5f93a
Jose Adrian Marin <jose.adrian.marin@correounivalle.edu.co>
2025-11-10 19:56:04 -0500

    Finished HU05 for WidgetInventory

--- app/src/main/java/com/univalle/widgetinventory/view/fragment/DetalleProductoFragment.kt
+++ app/src/main/java/com/univalle/widgetinventory/view/fragment/DetalleProductoFragment.kt
@@ -7,16 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-// import com.univalle.widgetinventory.data.AppDatabase
-// import com.univalle.widgetinventory.repository.ProductRepository
+import com.univalle.widgetinventory.R
 import com.univalle.widgetinventory.databinding.FragmentProductDetailsBinding
 import com.univalle.widgetinventory.viewModel.DetalleProductoViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class DetalleProductoFragment : Fragment() {
     private var _binding: FragmentProductDetailsBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: DetalleProductoViewModel by viewModels()
+
+    private var productId: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,19 +31,33 @@ class DetalleProductoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val productId = arguments?.getInt("product_id") ?: 0
+        productId = arguments?.getInt("product_id") ?: 0
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        binding.handler = this
+
         viewModel.cargarProducto(productId)
 
-        viewModel.producto.observe(viewLifecycleOwner) { product ->
-            binding.tvCodigoProducto.text = product.codigo.toString()
-            binding.tvNombreProducto.text = product.nombre
-            binding.tvPrecioProducto.text = product.precio.toString()
-            binding.tvCantidadProducto.text = product.cantidad.toString()
-        }
-
         binding.detalleProductoToolbar.setNavigationOnClickListener {
-            findNavController().navigateUp()
+            findNavController().navigate(R.id.homeFragment)
         }
+    }
+
+    fun onDeleteClick() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage("¿Desea eliminar el producto?")
+            .setNegativeButton("No", null)
+            .setPositiveButton("Si") { _, _ ->
+                viewModel.eliminarProducto(productId)
+                findNavController().navigate(R.id.homeFragment)
+            }
+            .show()
+    }
+
+    fun onEditClick() {
+        val bundle = Bundle().apply { putInt("product_id", productId) }
+        findNavController().navigate(R.id.editarProductoFragment, bundle)
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/univalle/widgetinventory/view/fragment/EditarProductoFragment.kt
@@ -0,0 +1,15 @@
+package com.univalle.widgetinventory.view.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.univalle.widgetinventory.R
+
+class EditarProductoFragment : Fragment(R.layout.fragment_editar_producto) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Esto es solo de prueba, al que le toque esta HU, puedes editar aqui xd
+    }
+}
+
